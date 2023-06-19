f025a35e5cfc999b6350ae7d5e0b2b6184149eb2
sas03 <stephanesumo@yahoo.fr>
2023-06-19 13:29:51 +0200

    Json Serializer

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace JSONChallenge;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string json = File.ReadAllText("weather.json");
+        WeatherInfo weatherinfo = JsonSerializer.Deserialize<WeatherInfo>(json);
+        Console.WriteLine("Longitude = " + weatherinfo.coordonnees.lon + "Latitude = " + weatherinfo.coordonnees.lat);
+        foreach(Weather weather in weatherinfo.weather)
+        {
+            Console.WriteLine("Id: " + weather.id + ", Main: " + weather.main + ", Description: " + weather.description + ", Icon: " + weather.icon);
+        }
+        Console.WriteLine("Base = " + weatherinfo.baseS);
+        Console.WriteLine("Main temp = " + weatherinfo.main.temp + ", Main pressure = " + weatherinfo.main.pressure + ", Main humidity = " + weatherinfo.main.humidity + ", Main temp_min = " + weatherinfo.main.temp_min + ", Main temp_max = " + weatherinfo.main.temp_max);
+        Console.WriteLine("Visibility = " + weatherinfo.visibility);
+        Console.WriteLine("Wind speed = " + weatherinfo.wind.speed + ", Wind deg = " + weatherinfo.wind.deg);
+        Console.WriteLine("Clouds all = " + weatherinfo.clouds.all);
+        Console.WriteLine("Dt = " + weatherinfo.dt);
+        Console.WriteLine("Sys type = " + weatherinfo.sys.type + ", Sys id = " + weatherinfo.sys.id + ", Sys message = " + weatherinfo.sys.message + ", Sys country = " + weatherinfo.sys.country + ", Sys sunrise = " + weatherinfo.sys.sunrise + ", Sys sunset = " + weatherinfo.sys.sunset);
+        Console.WriteLine("Weather Id = " + weatherinfo.id);
+        Console.WriteLine("Weather Name = " + weatherinfo.name);
+        Console.WriteLine("Weather Cod = " + weatherinfo.cod);
+    }
+}
---
+++ WeatherInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    public class WeatherInfo
+    {
+        [JsonPropertyName("coord")]
+        public Coord coordonnees { get; set; }
+        public List<Weather> weather { get; set; }
+        [JsonPropertyName("base")]
+        public string baseS { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
+    public class Coord
+    {
+        [JsonPropertyName("lon")]
+        public double lon { get; set; }
+        [JsonPropertyName("lat")]
+        public double lat { get; set; }
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double pressure { get; set; }
+        public double humidity { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+    public class Clouds
+    {
+        public double all { get; set; }
+    }
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public double message { get; set; }
+        public string country { get; set; }
+        public double sunrise { get; set; }
+        public double sunset { get; set; }
+    }
+}
